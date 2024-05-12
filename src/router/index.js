eeec307a4156eba89cd30f2e9cf6056e021df95f
Fadel <fadel.a.alobaidi@gmail.com>
2024-05-12 12:32:44 +0300

    testing files

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from '../components/MainPage.vue';
 import MyFreelancers from '../components/MyFreelancers.vue';
 import UserProfile from "../components/UserProfile.vue";
+import register from '../components/register.vue';
+import report from "../components/report.vue";
+import Login from '../components/login.vue';
 const routes = [
   {
     path: '/',
@@ -21,7 +24,22 @@ const routes = [
     path: "/UserProfile",
     name: "UserProfile",
     component: UserProfile
-  }
+  },
+  {    
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {    
+    path: "/register",
+    name: "register",
+    component: register
+  },
+  {    
+    path: "/report",
+    name: "report",
+    component: report
+  },
 ];
 const router = createRouter({
     history: createWebHistory(),
