d252fdc5f8657ea7f0cc5c4dce9400442e6b558e
Fadel <fadel.a.alobaidi@gmail.com>
2024-05-13 17:18:45 +0300

    freelancer controller

--- server/controllers/freelancer_controller.js
+++ server/controllers/freelancer_controller.js
@@ -1,15 +1,33 @@
-const Freelancer = require("../models/db_schema");
+const Freelancer = require("../models/db_schema").Freelancer;
+const getUsersById = require("./user_controller").getManyUsersByIds;
+
+async function createFreelancer(freelancerData){
+    return await Freelancer.create(freelancerData);
+}
+
+async function deleteFreelancerById(freelancerId){
+    return await Freelancer.deleteOne({_id: freelancerId});
+}
 
 async function getFreelancerById(freelancerId){
-    
+    return await Freelancer.findOne({_id: freelancerId});
+}
+
+async function getManyFreelancersByIds(freelancersId){
+    return await Freelancer.find({ _id: { $in: freelancerIds } });
 }
 
 async function getFreelancerReviews(freelancerId){
-
+    const freelancer = await getFreelancerById(freelancerId);
+    return await freelancer.reviews;
 }
+
 async function setAvatar(freelancerId){
-
+    const user = await getUserById(userId);
+    return user.save({avatar: imageSrc});
 }
+
 async function getEmployers(freelancerId){
 
 }
+module.exports = {getManyFreelancersByIds};
